f22ce703b33d50ce860b1af9ecb1b996114ecd3b
t5201314abc <t5201314abc@gmail.com>
2016-06-17 22:29:35 +0800

    Add files via upload

---
+++ RX_v2/RX_v2.ino
@@ -0,0 +1,82 @@
+char inData[24];
+byte index;
+boolean started = false;
+boolean ended = false;
+
+const int ledPin = 13;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Temperature & Humidity & UV Index");
+}
+
+void loop()
+{
+  while(Serial.available() > 0)
+  {
+    char aChar = Serial.read();
+    if(aChar == '<')
+    {
+      started = true;
+      index = 0;
+      inData[index] = '\0';
+    }
+    else if(aChar == '>')
+    {
+      ended = true;
+    }
+    else if(started)
+    {
+      inData[index] = aChar;
+      index++;
+      inData[index] = ',';
+    }
+  }
+
+  if(started && ended)
+  {
+    // Use the value
+    if(inData[0] == 'T')
+    {
+      inData[0] = ' ';
+      int windVal = atoi(inData);
+      Serial.println(" ");
+      Serial.print("Temp:");
+      Serial.print(inData);
+      Serial.print("C");
+      Serial.println(" ");
+      //digitalWrite(ledPin, HIGH);
+    }
+    else if(inData[0] == 'H')
+    {
+      inData[0] = ' ';
+      int temp = atoi(inData);
+      Serial.println(" ");
+      Serial.print("Humidity:");
+      Serial.print(inData);
+      Serial.print("%");
+      Serial.println(" ");
+      //digitalWrite(ledPin, HIGH);
+    }
+    else if(inData[0] == 'U')
+    {
+      inData[0] = ' ';
+      int uv = atoi(inData);
+      Serial.println(" ");
+      Serial.print("UV Index:");
+      Serial.print(inData);
+      //Serial.print("%");
+      Serial.println(" ");
+      //digitalWrite(ledPin, HIGH);
+    }
+
+    started = false;
+    ended = false;
+
+    index = 0;
+    inData[index] = '\0';
+    
+  }
+  //digitalWrite(ledPin, LOW);
+}
